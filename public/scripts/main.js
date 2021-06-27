ec195089569e45431d678459c0d774aa81c904c5
Christian Henrique <christianhmartins@gmail.com>
2021-06-27 19:09:06 -0300

    Final

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,40 +1,43 @@
-import Modal from './modal.js';
+import Modal from './modal.js'
 
-const modal = Modal();
+const modal = Modal()
 
-const modalTitle = document.querySelector('.modal h2');
-const modalDescription = document.querySelector('.modal p');
-const modalButton = document.querySelector('.modal button');
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
-// Quanto o botão check for clicado ele abre a modal 
 
-const checkButtons = document.querySelectorAll(".actions a.check");
+//Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check") 
 
-checkButtons.forEach(button =>{
-    button.addEventListener("click", handleClick);
+checkButtons.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener("click", handleClick)
 })
 
-// Quanto o botão delete for clicado ele abre a modal 
-const deleteButton = document.querySelectorAll(".actions a.delete");
 
-deleteButton.forEach(button =>{
-    button.addEventListener("click", (event) => handleClick(event, false));
+/*Quando o botão delete for clicado ele abre a modal */
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
 
 function handleClick(event, check = true){
-    event.preventDefault();
-    const text = check ? "Marcar como lido" : "Excluir";
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
 
-    modalTitle.innerHTML= `${text}`;
-    modalDescription.innerHTML= check ? `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?` : `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
-    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`; 
-    check? modalButton.classList.remove("red") : modalButton.classList.add("red");
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
-    const roomID = document.querySelector("#room-id").dataset.id;
-    const slug = check ? "check": "delete";
-    const questionID = event.target.dataset.id;
-    const form = document.querySelector(".modal form");
-    form.setAttribute("action", `/question/${roomID}/${questionID}/${slug}`);
-
-    modal.open();
+    modalTitle.innerHTML= `${text} esta pergunta`
+    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    
+    //abrir modal
+    modal.open()
 }
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,17 +1,17 @@
-export default function Modal(){  
-    
-    const modalWrapper = document.querySelector('.modal-wrapper');
-    const cancelButton = document.querySelector('.button.cancel');
+export default function Modal(){
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
 
     cancelButton.addEventListener("click", close)
-    
+
     function open(){
-        // Funcionalidade de abrir a modal
+        //funcionalidade de atribuir a classe active para a modal
         modalWrapper.classList.add("active")
     }
     function close(){
-        // Funcionalidade de fechar a modal
-        modalWrapper.classList.remove("active");
+        //funcionalidade de remover a classe active da modal
+        modalWrapper.classList.remove("active")
     }
 
     return{
